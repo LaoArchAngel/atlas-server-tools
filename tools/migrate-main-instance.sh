6d85896949cf65c3a9b5ee0c488b50819e0c9ee3
Brian Aldridge <brian@boisecomputerservice.com>
2019-01-17 17:09:10 -0700

    Update migrate-main-instance.sh

--- tools/migrate-main-instance.sh
+++ tools/migrate-main-instance.sh
@@ -3,8 +3,8 @@
 configfile="$1"
 instancefile="$2"
 
-if ! grep '^arkSingleInstance=' <"$configfile" >/dev/null 2>&1 && grep "^arkserverroot=" <"$configfile" >/dev/null 2>&1 && [ ! -f "$instancefile" ]; then
-  sed -n '/^#*\(ark\(\|flag\|opt\)_[^=]*\|arkserverroot\|serverMap\(\|ModId\)\)=/p' <"$configfile" >"$instancefile"
-  sed -i '/^ark\(serverroot\|_\(RCONPort\|Port\|QueryPort\)\)=/d' "$configfile"
+if ! grep '^atlasSingleInstance=' <"$configfile" >/dev/null 2>&1 && grep "^atlasserverroot=" <"$configfile" >/dev/null 2>&1 && [ ! -f "$instancefile" ]; then
+  sed -n '/^#*\(atlas\(\|flag\|opt\)_[^=]*\|atlasserverroot\|serverMap\(\|ModId\)\)=/p' <"$configfile" >"$instancefile"
+  sed -i '/^atlas\(serverroot\|_\(RCONPort\|Port\|QueryPort\)\)=/d' "$configfile"
   echo 'defaultinstance="main"' >>"$configfile"
 fi
