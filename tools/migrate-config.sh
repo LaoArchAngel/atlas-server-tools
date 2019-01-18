339d407e7c6137175cd3be3eff218687fb34f0e8
Brian Aldridge <brian@boisecomputerservice.com>
2019-01-17 17:08:39 -0700

    Update migrate-config.sh

--- tools/migrate-config.sh
+++ tools/migrate-config.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 configfile="$1"
-newopts=( arkbackupdir arkautorestartfile install_bindir install_libexecdir install_datadir mod_appid )
+newopts=( atlasbackupdir atlasautorestartfile install_bindir install_libexecdir install_datadir mod_appid )
 newopt_steamcmd_appinfocache="${PREFIX}/Steam/appcache/appinfo.vdf"
-newopt_arkbackupdir="${PREFIX}/ARK-Backups"
-newopt_arkautorestartfile="ShooterGame/Saved/.autorestart"
+newopt_atlasbackupdir="${PREFIX}/ATLAS-Backups"
+newopt_atlasautorestartfile="ShooterGame/Saved/.autorestart"
 newopt_install_bindir="${BINDIR}"
 newopt_install_libexecdir="${LIBEXECDIR}"
 newopt_install_datadir="${DATADIR}"
-newopt_mod_appid=346110
+newopt_mod_appid=834910
 
-if grep '^\(servermail\|arkstVersion\)=' "${configfile}" >/dev/null 2>&1; then
-  sed -i '/^\(servermail\|arkstVersion\)=/d' "${configfile}"
+if grep '^\(servermail\|atlasstVersion\)=' "${configfile}" >/dev/null 2>&1; then
+  sed -i '/^\(servermail\|atlasstVersion\)=/d' "${configfile}"
 fi
 
 for optname in "${newopts[@]}"; do
@@ -20,4 +20,3 @@ for optname in "${newopts[@]}"; do
     echo "${optname}='${!noptname}'" >>"${configfile}"
   fi
 done
-
