e1acf9fd48e4ff6244951f02f44e14688cd55a0e
Brian Aldridge <brian@boisecomputerservice.com>
2019-01-17 17:10:05 -0700

    Update uninstall.sh

--- tools/uninstall.sh
+++ tools/uninstall.sh
@@ -3,32 +3,32 @@
 # uninstall.sh
 
 BINDIR="/usr/bin"
-DATADIR="/usr/share/arkmanager"
-LIBEXECDIR="/usr/libexec/arkmanager"
+DATADIR="/usr/share/atlasmanager"
+LIBEXECDIR="/usr/libexec/atlasmanager"
 INITSCRIPT=
 
-if [ -f "/etc/rc.d/init.d/arkmanager" ]; then
-  INITSCRIPT="/etc/rc.d/init.d/arkmanager"
+if [ -f "/etc/rc.d/init.d/atlasmanager" ]; then
+  INITSCRIPT="/etc/rc.d/init.d/atlasmanager"
   if [ -f "/etc/rc.d/init.d/functions" ]; then
-    chkconfig arkmanager off
+    chkconfig atlasmanager off
   fi
-elif [ -f "/etc/init.d/arkmanager" ]; then
-  INITSCRIPT="/etc/init.d/arkmanager"
+elif [ -f "/etc/init.d/atlasmanager" ]; then
+  INITSCRIPT="/etc/init.d/atlasmanager"
   if [ -f "/lib/lsb/init-functions" ]; then
-    update-rc.d -f arkmanager remove
+    update-rc.d -f atlasmanager remove
   elif [ -f "/sbin/runscript" ]; then
-    rc-update del arkmanager default
+    rc-update del atlasmanager default
   fi
-elif [ -f "/etc/systemd/system/arkmanager.service" ]; then
-  INITSCRIPT="/etc/systemd/system/arkmanager.service"
-  systemctl disable arkmanager.service
+elif [ -f "/etc/systemd/system/atlasmanager.service" ]; then
+  INITSCRIPT="/etc/systemd/system/atlasmanager.service"
+  systemctl disable atlasmanager.service
 fi
 
 if [ -n "$INITSCRIPT" ]; then
   for f in "${INITSCRIPT}" \
-           "${BINDIR}/arkmanager" \
-           "${LIBEXECDIR}/arkmanager.init" \
-           "${LIBEXECDIR}/arkmanager-uninstall.sh"
+           "${BINDIR}/atlasmanager" \
+           "${LIBEXECDIR}/atlasmanager.init" \
+           "${LIBEXECDIR}/atlasmanager-uninstall.sh"
   do
     if [ -f "$f" ]; then
       rm "$f"
@@ -37,6 +37,6 @@ if [ -n "$INITSCRIPT" ]; then
 fi
 
 # remove bash_completion.d
-if [ -f "/etc/bash_completion.d/arkmanager" ]; then
-   rm "/etc/bash_completion.d/arkmanager"
+if [ -f "/etc/bash_completion.d/atlasmanager" ]; then
+   rm "/etc/bash_completion.d/atlasmanager"
 fi
