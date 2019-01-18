1e6712e5f379de353300b36901b0de3959129713
Brian Aldridge <brian@boisecomputerservice.com>
2019-01-17 17:09:36 -0700

    Update uninstall-user.sh

--- tools/uninstall-user.sh
+++ tools/uninstall-user.sh
@@ -3,9 +3,9 @@
 # uninstall-user.sh
 
 BINDIR="/home/steam/bin"
-DATADIR="/home/steam/.local/share/arkmanager"
+DATADIR="/home/steam/.local/share/atlasmanager"
 
-for f in "${BINDIR}/arkmanager" \
+for f in "${BINDIR}/atlasmanager" \
          "${DATADIR}/uninstall.sh"
 do
   if [ -f "$f" ]; then
